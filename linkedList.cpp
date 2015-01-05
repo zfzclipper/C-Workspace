2b6a3a5d081e593c2fa7be92b0bcba1d71b8773e
zfzclipper <zfzclipper@users.noreply.github.com>
2015-01-04 22:12:58 -0600

    update return front function

--- linkedList.cpp
+++ linkedList.cpp
@@ -1,8 +1,7 @@
 #include<iostream>
 using namespace std;
 
-class Node
-{
+class Node {
 public:
 	Node(void) : data(0), ptr_next(nullptr) {}
 	Node(int d, Node* pNxt = nullptr) : data(d), ptr_next(pNxt) { cout << "Construct Node" << endl; }
@@ -12,8 +11,7 @@ public:
 	Node*	ptr_next;
 };
 
-class List
-{
+class List {
 public:
 	List(void) : ptr_head(nullptr), ptr_tail(nullptr) { cout << "Default Constructor" << endl; }
 	List(Node* pHead, Node* pTail) : ptr_head(pHead), ptr_tail(pTail) {}
@@ -38,25 +36,21 @@ private:
 	Node*	ptr_tail;
 };
 
-List::~List()
-{
+List::~List() {
 	cout << "Destruct List" << endl;
 
-	while(ptr_head)
-	{
+	while (ptr_head) {
 		Node* ptr = ptr_head->ptr_next;
 		delete ptr_head;
 		ptr_head = ptr;
 	}
 }
 
-int List::size()
-{
+int List::size() {
 	int list_size = 0;
 	Node* ptr = ptr_head;
 
-	while(ptr)
-	{
+	while (ptr) {
 		list_size++;
 		ptr = ptr->ptr_next;
 	}
@@ -64,17 +58,17 @@ int List::size()
 	return list_size;
 }
 
-int List::front()
-{
-	if (ptr_head->ptr_next == nullptr) { exit(-1); }
+int List::front() {
+	if (!ptr_head) { 
+		exit(-1);
+	}
 
 	return ptr_head->data;
 }
 
-void List::push_front(const int& value)
-{
-	if (ptr_head == nullptr)
-	{
+#if 0
+void List::push_front(const int& value) {
+	if (ptr_head == nullptr) {
 		ptr_head = new Node(value, nullptr);
 		ptr_tail = ptr_head;
 		return;
@@ -82,11 +76,26 @@ void List::push_front(const int& value)
 
 	ptr_head = new Node(value, ptr_head);
 }
+#endif
 
-void List::pop_front(void)
-{
-	if (ptr_head == ptr_tail)
-	{
+void List::push_front(const int& value) {
+	ptr_head = new Node(value, ptr_head);
+
+	if (!ptr_tail) {
+		ptr_tail = ptr_head;
+	}
+}
+
+int List::back(void) {
+	if (!ptr_tail) {
+		exit(-1);
+	}
+
+	return ptr_tail->data; 
+}
+
+void List::pop_front(void) {
+	if (ptr_head == ptr_tail) {
 		ptr_tail = nullptr;
 	}
 
@@ -96,11 +105,9 @@ void List::pop_front(void)
 	delete front;
 }
 
-ostream& operator<<(ostream& os, const List& list)
-{
+ostream& operator<<(ostream& os, const List& list) {
 	Node* ptr = list.ptr_head;
-	while(ptr != list.ptr_tail)
-	{
+	while (ptr != list.ptr_tail) {
 		cout << ptr->data << ", ";
 		ptr = ptr->ptr_next;
 	}
@@ -110,18 +117,15 @@ ostream& operator<<(ostream& os, const List& list)
 }
 
 #if 1
-void List::remove(const int& value)
-{
-	if(!ptr_head) return;
+void List::remove(const int& value) {
+	if (!ptr_head) return;
 
 	Node* pre_ptr = new Node(0, ptr_head);
 	Node* cur_ptr = ptr_head;
 	Node* ptr_beforeHead = pre_ptr;
 
-	while(cur_ptr)
-	{
-		if(cur_ptr->data == value)
-		{
+	while (cur_ptr) {
+		if (cur_ptr->data == value) {
 			Node* temp_ptr = cur_ptr;
 
 			pre_ptr->ptr_next = cur_ptr->ptr_next;
@@ -129,8 +133,7 @@ void List::remove(const int& value)
 
 			delete temp_ptr;
 		}
-		else
-		{
+		else {
 			cur_ptr = cur_ptr->ptr_next;
 			pre_ptr = pre_ptr->ptr_next;
 		}
@@ -144,24 +147,20 @@ void List::remove(const int& value)
 #endif
 
 #if 0
-void List::remove(const int& value)
-{
+void List::remove(const int& value) {
 	if (!ptr_head) return;
 
 	Node* pre_ptr = new Node(0, ptr_head);
 	Node* before_front_ptr = pre_ptr;
 
-	while(pre_ptr->ptr_next)
-	{
-		if(pre_ptr->ptr_next->data == value)
-		{
+	while (pre_ptr->ptr_next) {
+		if (pre_ptr->ptr_next->data == value) {
 			Node* temp_ptr = pre_ptr->ptr_next;
 
 			pre_ptr->ptr_next = temp_ptr->ptr_next;
 			delete temp_ptr;
 		}
-		else
-		{
+		else {
 			pre_ptr = pre_ptr->ptr_next;
 		}
 	}
@@ -173,11 +172,10 @@ void List::remove(const int& value)
 } 
 #endif
 
-int main()
-{
+int main() {
+
 	List mylist;
-	for (int i = 0; i < 10; i++)
-	{
+	for (int i = 0; i < 10; i++) {
 		mylist.push_front(2*i+1);
 	}
 	
@@ -192,8 +190,7 @@ int main()
 	cout << mylist << endl;
 
 #if 0
-	while(mylist.size())
-	{
+	while (mylist.size()) {
 		mylist.pop_front();
 	}
 #endif
