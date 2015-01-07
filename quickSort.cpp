40a76cbed5ad10be0c29da9a794e0d4f5094ba9b
zfzclipper <zfzclipper@users.noreply.github.com>
2015-01-06 18:27:09 -0600

    quick sort first commit

--- quickSort.cpp
+++ quickSort.cpp
@@ -6,6 +6,106 @@ using namespace std;
 
 const int N = 18;
 
+void partition(vector<int>&, vector<int>&, int&, vector<int>&);
+void concatenate(vector<int>&, vector<int>&, int&, vector<int>&);
+
+void partition(list<int>&, list<int>&, int&, list<int>&);
+void concatenate(list<int>&, list<int>&, int&, list<int>&);
+
+void inplaceQuickSort(vector<int>& myArray) {
+	size_t N = myArray.size();
+
+	if (N <= 1) return;
+
+}
+
+
+void quickSort(vector<int>& myArray) {
+	if (myArray.size() <= 1) return;	//Base Case
+
+	vector<int> lessArray, greaterArray;
+	int pivot;
+
+	partition(myArray, lessArray, pivot, greaterArray);
+	quickSort(lessArray);
+	quickSort(greaterArray);
+	concatenate(myArray, lessArray, pivot, greaterArray);
+}
+
+void partition(vector<int>& myArray, vector<int>& lessArray, int& pivot, vector<int>& greaterArray) {
+	pivot = myArray.back();
+	myArray.pop_back();
+
+	while (!myArray.empty()) {
+		int temp = myArray.back();
+
+		if (temp < pivot) {
+			lessArray.push_back(temp);
+		}
+		else {
+			greaterArray.push_back(temp);
+		}
+
+		myArray.pop_back();
+	}
+}
+
+void concatenate(vector<int>& myArray, vector<int>& lessArray, int& pivot, vector<int>& greaterArray) {
+	if (!myArray.empty()) {
+		myArray.clear();
+	}
+
+	myArray = lessArray;
+	myArray.push_back(pivot);
+	for (auto& e : greaterArray) {
+		myArray.push_back(e);
+	}
+
+}
+
+void quickSort(list<int>& myList) {
+	if (myList.size() <= 1) return;
+
+	list<int> lessList, greaterList;
+	int pivot;
+
+	partition(myList, lessList, pivot, greaterList);
+	quickSort(lessList);
+	quickSort(greaterList);
+	concatenate(myList, lessList, pivot, greaterList);
+}
+
+void partition(list<int>& myList, list<int>& lessList, int& pivot, list<int>& greaterList) {
+	pivot = myList.back();
+	myList.pop_back();
+
+	while (!myList.empty()) {
+		int temp = myList.back();
+
+		if (temp < pivot) {
+			lessList.push_back(temp);
+		}
+		else {
+			greaterList.push_back(temp);
+		}
+
+		myList.pop_back();
+	}
+}
+
+void concatenate(list<int>& myList, list<int>& lessList, int& pivot, list<int>& greaterList) {
+	if (!myList.empty()) {
+		myList.clear();
+	}
+
+	myList = lessList;
+	myList.push_back(pivot);
+	for (auto& e : greaterList) {
+		myList.push_back(e);
+	}
+
+}
+
 int main() {
 	list<int> mylist;
 	vector<int> myArray;
@@ -39,7 +139,7 @@ int main() {
 		cout << myArray[i] << ", ";
 	}
 
-	//mergeSort(mylist);
+	quickSort(mylist);
 
 	cout << "Sorted list: ";	
 	for (auto it = mylist.begin(); it != mylist.end(); it++) {
@@ -50,7 +150,7 @@ int main() {
 		cout << *it << ", ";
 	}
 
-	//mergeSort(myArray);
+	quickSort(myArray);
 
 	cout << "Sorted array: ";
 	for (size_t i = 0; i < myArray.size(); i++) {
